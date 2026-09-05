5d13d08bdce12b672571b08c37c3bb5037c36338
Mohit <mt7512590@gmail.com>
2026-09-05 20:14:25 +0530

    updates

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,7 @@ import About from "@/components/About";
 import Achievements from "@/components/Achievements";
 import Contact from "@/components/Contact";
 import Footer from "@/components/Footer";
+import Experience from "@/components/Experience";
 
 export default function Home() {
   return (
@@ -16,6 +17,7 @@ export default function Home() {
         <About />
         <Achievements />
         <Contact />
+        <Experience />
       </main>
       <Footer />
     </>
